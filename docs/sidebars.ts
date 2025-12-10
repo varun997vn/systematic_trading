160ae0439d99cd2b7e3b2e424c2bbfae21bf47aa
Shubham Kaushal <35777753+shubhamkaushal765@users.noreply.github.com>
2025-12-10 13:52:33 +0800

    Database: Add a dustbin to the project (#4)

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -2,10 +2,10 @@ import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
   systematicTradingSidebar: [
-    'systematic-trading/trader',              // appears last
+    'systematic-trading/trader',
     'systematic-trading/data-manager',
     'systematic-trading/strategies',
-    'systematic-trading/package-architecture', // appears first
+    'systematic-trading/package-architecture',
     'systematic-trading/database',
   ],
 };
